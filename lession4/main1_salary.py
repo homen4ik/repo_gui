b2486c88f8df4a8df38e6f4e200d1a689989a748
Swetlana <7sweta13@mail.ru>
2021-10-10 11:21:20 +0300

    dz4

---
+++ lession4/main1_salary.py
@@ -0,0 +1,9 @@
+def salary():
+    my_salary = int(time) * int(rate) + int(bonus)
+    return my_salary
+
+
+time = input('введите количество часов: ')
+rate = input('введите ставку в час: ')
+bonus = input('введите размер премии: ')
+
